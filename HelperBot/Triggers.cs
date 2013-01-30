de91c179ba55319ec162e7e7475ab8882b17564c
LeChosenOne <jonahw@optonline.net>
2013-01-29 21:31:32 -0500

    thanks triggers

--- HelperBot/Triggers.cs
+++ HelperBot/Triggers.cs
@@ -26,14 +26,17 @@ namespace HelperBot {
 
             return true;
         }
+        public static DateTime SinceAnswer;
 
         public static void CheckTriggers ( Player player, String Message, MessageChannel Channel ) {
             if ( Channel == MessageChannel.PM ) return;
             if ( Triggers.MatchesTrigger( Message, RankTriggers.NextRankFullTrigger ) ) {
                 if ( player.Info.Rank != RankManager.HighestRank ) {
                     Methods.SendMessage( player.ClassyName + "&F, your next rank is " + player.Info.Rank.NextRankUp.ClassyName, Channel );
+                    SinceAnswer = DateTime.Now;
                 } else {
                     Methods.SendMessage( player.ClassyName + "&F, you are already the highest rank!", Channel );
+                    SinceAnswer = DateTime.Now;
                 }
                 return;
             }
@@ -46,42 +49,51 @@ namespace HelperBot {
                 if ( player.Info.Rank == RankManager.HighestRank ) return;
                 if ( player.Can( Permission.ReadStaffChat ) ) {
                     Methods.SendMessage( player.ClassyName + "&f, " + Settings.HowToGetRankedStaffString, Channel );
+                    SinceAnswer = DateTime.Now;
                 } else {
                     Methods.SendMessage( player.ClassyName + "&f, " + Settings.HowToGetRankedBuilderString, Channel );
+                    SinceAnswer = DateTime.Now;
                 }
                 return;
             }
             if (Triggers.MatchesTrigger(Message, MaintenanceTriggers.PMFullTrigger))
             {
                 Methods.SendMessage(player.ClassyName + "&f, to PM, type '@playername [message]'.", Channel);
+                SinceAnswer = DateTime.Now;
                 return;
             }
             if ( Triggers.MatchesTrigger( Message, MaintenanceTriggers.TimeFullTrigger ) ) {
                 Methods.SendMessage( player.ClassyName + "&f, the time is currently " + DateTime.Now.ToShortTimeString(), Channel );
+                SinceAnswer = DateTime.Now;
                 return;
             }
             if ( Triggers.MatchesTrigger( Message, MaintenanceTriggers.FellFullTrigger ) ) {
                 Methods.SendMessage( player.ClassyName + Settings.StuckMessage, Channel );
+                SinceAnswer = DateTime.Now;
                 return;
             }
             if ( Triggers.MatchesTrigger( Message, MaintenanceTriggers.HoursFullTrigger ) ) {
                 Methods.SendMessage( Methods.GetPlayerTotalHoursString( player ), Channel );
+                SinceAnswer = DateTime.Now;
                 return;
             }
             if ( File.Exists( "SwearWords.txt" ) ) {
                 if ( !player.Can( Permission.Swear ) ) {
                     if ( Triggers.MatchesTrigger( Message, MaintenanceTriggers.SwearFullTrigger ) ) {
                         Methods.SendMessage( player, "Please refrain from swearing :)", MessageChannel.PM );
+                        SinceAnswer = DateTime.Now;
                         return;
                     }
                 }
             }
             if ( Triggers.MatchesTrigger( Message, MaintenanceTriggers.WebFullTrigger ) ) {
                 Methods.SendMessage( player.ClassyName + "&F, the server's website is " + Settings.Website, Channel );
+                SinceAnswer = DateTime.Now;
                 return;
             }
             if ( Triggers.MatchesTrigger( Message, MaintenanceTriggers.ServFullTrigger ) ) {
                 Methods.SendMessage( player.ClassyName + "&F, you are currently playing on " + ConfigKey.ServerName.GetString(), Channel );
+                SinceAnswer = DateTime.Now;
                 return;
             }
             if ( Triggers.MatchesTrigger( Message, MiscTriggers.SpleefFullTrigger ) ) {
@@ -107,7 +119,21 @@ namespace HelperBot {
             }
             if ( Triggers.MatchesTrigger( Message, MiscTriggers.FlyFullTrigger ) ) {
                 Methods.SendMessage( player.ClassyName + "&F, to fly, type /fly, or download WoM at womjr.com/game_client.", MessageChannel.Global );
+                SinceAnswer = DateTime.Now;
                 return;
+            }
+            if (Triggers.MatchesTrigger(Message, MiscTriggers.ThanksFullTrigger))
+            {
+                if(SinceAnswer.Millisecond < 20000)
+                {
+                    String[] thanksArray = { "Np",
+                                               "Anytime",
+                                               "Always here for you",
+                                               "^^",
+                                               "Yw" };
+                    Random thanksMsg = new Random();
+                    Methods.SendMessage("&f, " + thanksArray[thanksMsg.Next(0, thanksArray.Length)], MessageChannel.Global);
+                }
             }
         }
 
