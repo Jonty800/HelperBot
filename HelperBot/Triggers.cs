91197c6f5fa48c9e78e2b781de58b96abe76498b
LeChosenOne <jonahw@optonline.net>
2013-02-23 21:21:20 -0500

    config stuff

--- HelperBot/Triggers.cs
+++ HelperBot/Triggers.cs
@@ -16,10 +16,10 @@ namespace HelperBot {
     public static class Triggers {
 
         public static bool SpleefInProgress = false;
-        public static bool IsAllUpper(string text)
+        public static bool IsAllUpper(String text)
         {
             for (int i = 0; i < text.Length; i++)
-            {
+            { 
                 if (!Char.IsUpper(text[i]))
                     return false;
             }
@@ -30,10 +30,13 @@ namespace HelperBot {
         public static void CheckTriggers ( Player player, String Message, MessageChannel Channel ) {
             //Check swears in PMs
             if ( Channel == MessageChannel.PM ) {
-                if ( File.Exists( "SwearWords.txt" ) ) {
-                    if ( !player.Can( Permission.Swear ) ) {
-                        if ( Triggers.MatchesTrigger( Message, MaintenanceTriggers.SwearFullTrigger ) ) {
-                            Methods.SendMessage( player, Color.PM + "Please refrain from swearing :)", MessageChannel.PM );
+                if (File.Exists("SwearWords.txt") && Settings.AnnounceWarnSwear)
+                {
+                    if (!player.Can(Permission.Swear))
+                    {
+                        if (Triggers.MatchesTrigger(Message, MaintenanceTriggers.SwearFullTrigger))
+                        {
+                            Methods.SendMessage(player, Color.PM + "Please refrain from swearing :)", MessageChannel.PM);
                             return;
                         }
                     }
@@ -42,7 +45,7 @@ namespace HelperBot {
             if ( Channel == MessageChannel.PM ) return; //ignore PMs for this
 
             //What is my next rank?
-            if ( Triggers.MatchesTrigger( Message, RankTriggers.NextRankFullTrigger ) ) {
+            if ( Triggers.MatchesTrigger( Message, RankTriggers.NextRankFullTrigger ) && Settings.AnnounceRank ) {
                 if ( player.Info.Rank != RankManager.HighestRank ) {
                     Methods.SendMessage( player.ClassyName + "&F, your next rank is " + player.Info.Rank.NextRankUp.ClassyName, Channel );
                     Methods.AddTYPlayer( player );
@@ -52,9 +55,14 @@ namespace HelperBot {
                 }
                 return;
             }
-
+            //Caps checker
+            if (IsAllUpper(Message))
+            {
+                Methods.SendPM(player, "Please refrain from abusing caps.");
+            }
             //How do I get the next rank?
-            if ( Triggers.MatchesTrigger( Message, RankTriggers.HowDoFullTrigger ) ) {
+            if (Triggers.MatchesTrigger(Message, RankTriggers.HowDoFullTrigger) && Settings.AnnounceRank)
+            {
                 if ( player.Info.Rank == RankManager.HighestRank ) return;
                 if ( player.Can( Permission.ReadStaffChat ) ) {
                     Methods.SendMessage( player.ClassyName + "&f, " + Settings.HowToGetRankedStaffString, Channel );
@@ -67,49 +75,49 @@ namespace HelperBot {
             }
 
             //How do I PM players?
-            if ( Triggers.MatchesTrigger( Message, MaintenanceTriggers.PMFullTrigger ) ) {
+            if ( Triggers.MatchesTrigger( Message, MaintenanceTriggers.PMFullTrigger ) && Settings.AnnouncePM) {
                 Methods.SendMessage( player.ClassyName + "&f, to PM, type '@playername [message]'.", Channel );
                 Methods.AddTYPlayer( player );
                 return;
             }
 
             //What is the time
-            if ( Triggers.MatchesTrigger( Message, MaintenanceTriggers.TimeFullTrigger ) ) {
+            if ( Triggers.MatchesTrigger( Message, MaintenanceTriggers.TimeFullTrigger ) && Settings.AnnounceTime) {
                 Methods.SendMessage( player.ClassyName + "&f, the server's time is currently " + DateTime.Now.ToShortTimeString(), Channel );
                 Methods.AddTYPlayer( player );
                 return;
             }
 
             //I fell
-            if ( Triggers.MatchesTrigger( Message, MaintenanceTriggers.FellFullTrigger ) ) {
+            if ( Triggers.MatchesTrigger( Message, MaintenanceTriggers.FellFullTrigger ) && Settings.AnnounceFell) {
                 Methods.SendMessage( player.ClassyName + Settings.StuckMessage, Channel );
                 Methods.AddTYPlayer( player );
                 return;
             }
 
             //What are my hours?
-            if ( Triggers.MatchesTrigger( Message, MaintenanceTriggers.HoursFullTrigger ) ) {
-                Methods.SendMessage(Methods.GetPlayerTotalHoursString(player), Channel );
+            if ( Triggers.MatchesTrigger( Message, MaintenanceTriggers.HoursFullTrigger ) && Settings.AnnounceHours) {
+                Methods.SendMessage(Methods.GetPlayerTotalHoursString(player) , Channel);
                 Methods.AddTYPlayer( player );
                 return;
             }
             
             //What is the website?
-            if ( Triggers.MatchesTrigger( Message, MaintenanceTriggers.WebFullTrigger ) ) {
+            if ( Triggers.MatchesTrigger( Message, MaintenanceTriggers.WebFullTrigger )) {
                 Methods.SendMessage( player.ClassyName + "&F, the server's website is " + Settings.Website, Channel );
                 Methods.AddTYPlayer( player );
                 return;
             }
 
             //What is the server's name?
-            if ( Triggers.MatchesTrigger( Message, MaintenanceTriggers.ServFullTrigger ) ) {
+            if ( Triggers.MatchesTrigger( Message, MaintenanceTriggers.ServFullTrigger ) && Settings.AnnounceServerName) {
                 Methods.SendMessage( player.ClassyName + "&F, you are currently playing on " + ConfigKey.ServerName.GetString(), Channel );
                 Methods.AddTYPlayer( player );
                 return;
             }
 
             //!Spleef
-            if ( Triggers.MatchesTrigger( Message, MiscTriggers.SpleefFullTrigger ) ) {
+            if ( Triggers.MatchesTrigger( Message, MiscTriggers.SpleefFullTrigger ) && Settings.AnnounceSpleefTimer ) {
                 if ( SpleefInProgress ) {
                     Methods.SendPM( player, "There is already a spleef timer!" );
                     return;
@@ -131,20 +139,20 @@ namespace HelperBot {
             }
 
             //Alice, Joke
-            if ( Triggers.MatchesNameAndTrigger( Message, MiscTriggers.JokeFullTrigger ) ) {
+            if ( Triggers.MatchesNameAndTrigger( Message, MiscTriggers.JokeFullTrigger ) && Settings.AnnounceJokes) {
                 Methods.SendMessage( Methods.GetRandomJoke(), MessageChannel.Global );
                 return;
             }
 
             //how do I fly? Using CM now since Java 7 is such a pain
-            if ( Triggers.MatchesTrigger( Message, MiscTriggers.FlyFullTrigger ) ) {
+            if ( Triggers.MatchesTrigger( Message, MiscTriggers.FlyFullTrigger ) && Settings.AnnounceFly) {
                 Methods.SendMessage( player.ClassyName + "&F, to fly, type /fly, or download CM at http://is.gd/charged.", MessageChannel.Global );
                 Methods.AddTYPlayer( player );
                 return;
             }
 
             //fun fact
-            if ( Triggers.MatchesNameAndTrigger( Message, MiscTriggers.FunFactFullTrigger ) ) {
+            if ( Triggers.MatchesNameAndTrigger( Message, MiscTriggers.FunFactFullTrigger ) && Settings.AnnounceJokes) {
                 Methods.SendMessage( Methods.GetRandomStatString( player ), Channel );
                 return;
             }
